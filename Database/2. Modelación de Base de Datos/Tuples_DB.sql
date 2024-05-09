bebc4033405b3a29d4e0d2a08d8ccc03ed8c06d1
Fabrizio Barrios Blanco <a01784901@tec.mx>
2024-05-08 20:06:16 -0600

    Progress on tuple filling from the issue #12

--- Database/2. Modelación de Base de Datos/Tuples_DB.sql
+++ Database/2. Modelación de Base de Datos/Tuples_DB.sql
@@ -1,16 +1,46 @@
 USE submil;
 
 INSERT INTO Type_ (Name) VALUES
+('Heathcliff'),
 ('Faust'),
-('Mephistopheles');
+('Don Quixote'),
+('Ishmael'),
+('Outis'),
+('Yi Sang'),
+('Opportunistic Slash'),
+('Blunt Hit'),
+('Weak Punch'),
+('Strong Block'),
+('Shield'),
+('Arm Block'),
+('Ego Armor'),
+('Ego Weapon'),
+('Ego Needie'),
+('Healing Ampule'),
+('Ego Claw');
 
 INSERT INTO Deck (Name, Creation_Date, Description, Type, Size) VALUES
 ('Basic Deck 1', NOW(), 'Description Deck 1', 'Type 1', 23),
 ('Basic Deck 2', NOW(), 'Description Deck 2', 'Type 2', 23);
 
 INSERT INTO Card (Type_ID, Name, Description) VALUES
-(1, 'Faust', 'Description Card 1'),
-(2, 'Mephistopheles', 'Description Card 2');
+(1, 'Heathcliff', 'Tank Unit'),
+(1, 'Faust', 'Balanced Unit'),
+(1, 'Don Quixote', 'Fast Unit'),
+(1, 'Ishmael', 'Tank Unit'),
+(1, 'Outis', 'Balanced Unit'),
+(1, 'Yi Sang', 'Fast Unit'),
+(2, 'Opportunistic Slash', 'A strong slash with powerful damage'),
+(2, 'Blunt Hit', 'A hard hit that knocks back enemies'),
+(2, 'Weak Punch', 'A weak preemptive hit'),
+(3, 'Strong Block', 'A strong block that covers vitals'),
+(3, 'Shield', 'A shield that can support a medium hit'),
+(3, 'Arm Block', 'A desperate struggle to avoid damage'),
+(4, 'Ego Armor', 'Powerful EGO gear that allows for greater defense'),
+(4, 'Ego Weapon', 'Powerful EGO gear that allows for greater power'),
+(4, 'Ego Needie', 'Powerful EGO gear that allows piercing defense'),
+(4, 'Healing Ampule', 'Description 1'),
+(5, 'Ego Claw', 'A claw that allows for deeper insight');
 
 INSERT INTO Player (Deck_ID, Name, Registration_Date, Password, IsNPC) VALUES
 (1, 'Fabrizio', NOW(), 'password1', FALSE),
@@ -34,7 +64,6 @@ INSERT INTO Card_Stats (Card_ID, Stats_ID) VALUES
 (1, 1),
 (2, 2);
 
-SELECT * FROM Match_;
 
 
 
